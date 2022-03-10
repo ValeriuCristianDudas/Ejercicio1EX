f6370f2bdd8f4d2f8ab4e12b7c8083a26a37379c
ValeriuCristianDudas <valeriucristiandudas@gmail.com>
2022-03-10 17:16:39 +0100

    commit0

---
+++ src/Ejercicio1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+public static void main(String[] args) {
+		
+		Scanner capt = new Scanner(System.in);
+		
+		System.out.print("Ingrese numero: ");
+		int a = capt.nextInt();
+		//0
+		System.out.print((a) + "*" + (0) + " = " + (a*0) + "\n");
+		//1
+		System.out.print((a) + "*" + (1) + " = " + (a*1) + "\n");
+		//2
+		System.out.print((a) + "*" + (2) + " = " + (a*2) + "\n");
+		//3
+		System.out.print((a) + "*" + (3) + " = " + (a*3) + "\n");
+		//4
+		System.out.print((a) + "*" + (4) + " = " + (a*4) + "\n");
+		//5
+		System.out.print((a) + "*" + (5) + " = " + (a*5) + "\n");
+		//6
+		System.out.print((a) + "*" + (6) + " = " + (a*6) + "\n");
+		//7
+		System.out.print((a) + "*" + (7) + " = " + (a*7) + "\n");
+		//8
+		System.out.print((a) + "*" + (8) + " = " + (a*8) + "\n");
+		//9
+		System.out.print((a) + "*" + (9) + " = " + (a*9) + "\n");
+		//10
+		System.out.print((a) + "*" + (10) + " = " + (a*10) + "\n");
+			
+	}
+}
